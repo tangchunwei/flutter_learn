614635cb1edaaa4b187622704842c8d84c474151
淡定 <33542476+tangchunwei@users.noreply.github.com>
2021-12-12 03:04:59 +0800

    #StatefulWidget练习

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: HomePage()
-    );
+    return const MaterialApp(home: HomePage());
   }
 }
-class HomePage extends StatelessWidget{
+
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title:const Text("我是淡定"),
-          centerTitle:true,
-          elevation:100.0
-      ),
+          title: const Text("我是淡定"), centerTitle: true, elevation: 100.0),
+      body: const CountPage(),
     );
   }
 }
 
+class CountPage extends StatefulWidget {
+  const CountPage({Key? key}) : super(key: key);
+  @override
+  _CountPageState createState() => _CountPageState();
+}
+
+class _CountPageState extends State<CountPage> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("$count"),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                count++;
+              });
+            },
+            child: const Text('点击'))
+      ],
+    );
+  }
+}
