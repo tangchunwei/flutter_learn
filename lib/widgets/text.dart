d18431cab997d9906f8f3df22eed9686ab0e2d57
淡定 <33542476+tangchunwei@users.noreply.github.com>
2021-12-12 11:07:37 +0800

    #demo目录

---
+++ lib/widgets/text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TextDemo extends StatelessWidget {
+  const TextDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('测试');
+  }
+}
