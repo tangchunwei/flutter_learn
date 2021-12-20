d6e816167cfed79e54c9c9d8ab26f20b93b81538
淡定 <33542476+tangchunwei@users.noreply.github.com>
2021-12-21 01:41:44 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,28 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
           title: const Text("我是淡定"), centerTitle: true, elevation: 100.0),
       body: const TextDemo(),
+      drawer: const MyDrawer(),
+    );
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: MediaQuery.removePadding(
+          context: context,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(top: 70),
+                child: Text("data"),
+              )
+            ],
+          )),
     );
   }
 }
